768a0dc6f40ead962ba51d69f2831e6178f72ff0
Clancy Emanuel <clancy.emanuel@gmail.com>
2017-04-02 09:15:43 -0400

    Added setup method to create the leds for test fixture

--- LedDriver/test/LedDriverTest.cpp
+++ LedDriver/test/LedDriverTest.cpp
@@ -6,24 +6,24 @@ class ledDriverTest : public ::testing::Test
 {
 protected:
 	uint16_t virtualLeds;
+	void SetUp() {
+		LedDriver_Create(&virtualLeds);
+	}
 };
 
 TEST_F(ledDriverTest, LedsOffAfterCreate)
 {
-	LedDriver_Create(&virtualLeds);
 	ASSERT_EQ(0, virtualLeds);
 }
 
 TEST_F(ledDriverTest, TurnOnLedOne)
 {
-	LedDriver_Create(&virtualLeds);
 	LedDriver_TurnOn(1);
 	ASSERT_EQ(1, virtualLeds);
 }
 
 TEST_F(ledDriverTest, TurnOffLedOne)
 {
-	LedDriver_Create(&virtualLeds);
 	LedDriver_TurnOn(1);
 	LedDriver_TurnOff(1);
 	ASSERT_EQ(0,virtualLeds);
